d8d1fce7e01afa0268df04d3de31b53546ec007d
danieltudares <dtudares@gmail.com>
2020-06-14 12:03:02 -0400

    Adding ultra challenge mode

--- coffee_n_code/rock_paper_scissors.py
+++ coffee_n_code/rock_paper_scissors.py
@@ -72,6 +72,33 @@ def rock_paper_scissors(player1,player2):
         return 'invalid input'
 
 
+
+#ultra challenge mode with user input:
+def rock_paper_scissors(player1,player2):
+    if (player1 == 'paper' and player2 == 'scissors') or (player1 == 'scissors' and player2 == 'paper'):
+        print('scissors win!')
+    elif (player1 == 'scissors' and player2 == 'rock') or (player1 == 'rock' and player2 == 'scissors'):
+        print('rock wins!')
+    elif (player1 == 'rock' and player2 == 'paper') or (player1 == 'paper' and player2 == 'rock'):
+        print('paper wins!')
+    elif player1 == 'rock' and player2 == 'rock':
+        print('draw!')
+    elif player1 == 'paper' and player2 == 'paper':
+        print('draw!')
+    elif player1 == 'scissors' and player2 == 'scissors':
+        print('draw!')
+    else:
+        print('invalid input')
+
+print("Player 1 - Enter your option (options: paper, rock, scissors)")
+player1 = input()
+print("Player 2 - Enter your option (options: paper, rock, scissors)")
+player2 = input()
+rock_paper_scissors(player1,player2)
+
+
+
+
 print(rock_paper_scissors('paper', 'scissors') == 'scissors win!')
 print(rock_paper_scissors('scissors', 'rock') == 'rock wins!')
 print(rock_paper_scissors('rock', 'paper') == 'paper wins!')
