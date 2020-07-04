67b10c9a6a7702f522961d6a29ea4ad459aeeb60
Daniel Tudares <daniel.tudares@shopify.com>
2020-07-04 10:32:12 -0400

    Adding extra lines for better readability

--- coffee_n_code/rock_paper_scissors-reloaded.py
+++ coffee_n_code/rock_paper_scissors-reloaded.py
@@ -17,7 +17,7 @@ def rock_paper_scissors():
     score_p2 = 0
     player1 = input("Player 1 name: ")
     player2 = input("Player 2 name: ")
-    print("Welcome to rock, paper, scissors! - The first to win 3 rounds is the champion!")
+    print("\nWelcome to rock, paper, scissors! - The first to win 3 rounds is the champion!\n")
 
     while score_p1 <= 3 and score_p2 <= 3:
         if score_p1 == 3:
@@ -26,41 +26,41 @@ def rock_paper_scissors():
         elif score_p2 == 3:
             print(player2 + " won 3 rounds, congratulations!!")   
             break 
-        print("Current Score: " + player1 + " = " + str(score_p1) + " ," + player2 + " = " + str(score_p2))
-        select_p1 = getpass( player1 + " ,pick your weapon (rock, paper, scissors): ").lower()
-        select_p2 = getpass( player2 + " ,pick your weapon (rock, paper, scissors): ").lower()
+        print("Current Score: " + player1 + " = " + str(score_p1) + " ," + player2 + " = " + str(score_p2) + "\n")
+        select_p1 = getpass( player1 + ", pick your weapon (rock, paper, scissors): ").lower()
+        select_p2 = getpass( player2 + ", pick your weapon (rock, paper, scissors): ").lower()
         if select_p1 == select_p2:
-            print("It's a draw! - No points, Try again")
+            print("\n It's a draw! - No points, Try again\n")
         elif select_p1 == "paper":
             if select_p2 == "scissors":
-                print("scissors wins! - Point for " + player2 )
+                print("\n scissors wins! - Point for " + player2 + "\n")
                 score_p2 += 1
             elif select_p2 == "rock":
-                print("paper wins! - Point for " + player1 )
+                print("\n paper wins! - Point for " + player1 + "\n")
                 score_p1 += 1
             else:
-                print(player2 + " ,you made an invalid input - Try again")
+                print("\n " + player2 + " ,you made an invalid input - Try again \n")
         elif select_p1 == "scissors":
             if select_p2 == "paper":
-                print("scissors wins! - Point for " + player1 )
+                print("\n scissors wins! - Point for " + player1 + "\n")
                 score_p1 += 1
             elif select_p2 == "rock":
-                print("rock wins! - Point for " + player2 )
+                print("\n rock wins! - Point for " + player2 + "\n")
                 score_p2 += 1
             else:
-                print(player2 + " ,you made and invalid input - Try again")
+                print("\n " + player2 + " ,you made and invalid input - Try again \n")
         elif select_p1 == "rock":
             if select_p2 == "scissors":
-                print("rock wins! - Point for " + player1)
+                print("\n rock wins! - Point for " + player1 + "\n")
                 score_p1 += 1
             elif select_p2 == "paper":
-                print("paper wins! - Point for " + player2)
+                print("\n paper wins! - Point for " + player2 + "\n")
                 score_p2 += 1
             else:
-                print(player2 + " ,you made an invalid input - Try again")
+                print("\n " + player2 + " ,you made an invalid input - Try again \n")
         elif ((select_p1 != "paper") or (select_p1 != "scissors") or (select_p1 != "rock")) and ((select_p2 == "paper") or (select_p2 == "scissors") or (select_p2 == "rock")):
-            print(player1 + " ,you made an invalid input - Try again")
+            print("\n " + player1 + " ,you made an invalid input - Try again \n")
         else:
-            print("both users invalid input - Try again")
+            print("\n both users invalid input - Try again \n")
 
 rock_paper_scissors()
